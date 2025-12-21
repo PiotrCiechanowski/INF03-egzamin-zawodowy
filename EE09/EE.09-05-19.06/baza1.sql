08608f632ac6f0a1f5fc7c5eeca5151abe47d336
Piotr Ciechanowski <piotrekciechanowski2222@gmail.com>
2025-12-21 21:27:21 +0100

    Add files via upload

---
+++ EE09/EE.09-05-19.06/baza1.sql
@@ -0,0 +1,93 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 23 Mar 2023, 15:47
+-- Wersja serwera: 10.4.27-MariaDB
+-- Wersja PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `ciagi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `matematycy`
+--
+
+CREATE TABLE `matematycy` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `imie` text DEFAULT NULL,
+  `nazwisko` text DEFAULT NULL,
+  `rok_urodzenia` int(10) DEFAULT NULL,
+  `liczba_publikacji` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Zrzut danych tabeli `matematycy`
+--
+
+INSERT INTO `matematycy` (`id`, `imie`, `nazwisko`, `rok_urodzenia`, `liczba_publikacji`) VALUES
+(1, 'Stefan', 'Banach', 1892, 20),
+(2, 'Leonardo', 'Fibonacci', 1175, 15),
+(3, 'Augustin', 'Cauchy', 1789, 10),
+(4, 'Leonard', 'Euler', 1707, 30),
+(5, 'Gotfried', 'Leibniz', 1646, 40);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `typy`
+--
+
+CREATE TABLE `typy` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `kategoria` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Zrzut danych tabeli `typy`
+--
+
+INSERT INTO `typy` (`id`, `kategoria`) VALUES
+(1, 'Procesor'),
+(2, 'RAM'),
+(5, 'karta graficzna'),
+(6, 'HDD');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `matematycy`
+--
+ALTER TABLE `matematycy`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `matematycy`
+--
+ALTER TABLE `matematycy`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
